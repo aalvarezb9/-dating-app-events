3987238488c5ddfc1286a8880153b099cd68fe55
Angel Alvarez <anrealba9@gmail.com>
2026-07-24 18:13:51 -0600

    Add .gitignore and fix package.json dependencies

--- dist/events.module.d.ts
+++
@@ -1,7 +0,0 @@
-import { DynamicModule } from '@nestjs/common';
-import { EventPublisherConfig } from './services/event-publisher.service';
-export declare class EventsModule {
-    static register(config: EventPublisherConfig): DynamicModule;
-    static registerAsync(configFactory: () => Promise<EventPublisherConfig>): DynamicModule;
-}
-//# sourceMappingURL=events.module.d.ts.map
