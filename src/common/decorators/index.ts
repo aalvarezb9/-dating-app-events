1a44177beb98fa56f7c31789c302b1d182b267d6
Angel Alvarez <anrealba9@gmail.com>
2026-08-18 00:49:49 -0600

    some changes

--- src/common/decorators/index.ts
+++ src/common/decorators/index.ts
@@ -2,5 +2,6 @@ export * from './current-user.decorator';
 export * from './current-tenant.decorator';
 export * from './roles.decorator';
 export * from './public.decorator';
+export * from './cross-tenant.decorator';
 export * from './use-dto.decorator';
 export * from './api-standard-response.decorator';
